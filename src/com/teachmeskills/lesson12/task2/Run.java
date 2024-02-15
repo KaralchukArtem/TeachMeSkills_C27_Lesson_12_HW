4556270d888b86cd9375f010f67981d262859320
Karalchuk Tema <artuom441@gmail.com>
2024-02-15 18:51:21 +0300

    feat: task 2 phone number... phone number format +xxx(xx)xxxxxxx

--- src/com/teachmeskills/lesson12/task2/Run.java
+++ src/com/teachmeskills/lesson12/task2/Run.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class Run {
 
     public static void main(String[] args) {
-        String text = "sdg sdg sdg@ vbfgrSE ./ /o;  8&teachmeskills@gmail_.com8, ghk doc number JKI1423-1512-51LK phone number sdf e23 L+375(29)375-3318L";
-        String REG = "\\+\\d{3}?.(\\d{2})?.\\d{3}(-\\d{2}){2}|\\+\\d{3}?.(\\d{2})?.\\d{3}(-\\d{4})|[a-z0-9_-]+@[a-z0-9_\\.-]+\\.([a-z]{3})|(?:\\d{4}-){2}(\\d{2})";
+        String text = "sdg sdg sdg@ vbfgrSE ./ /o;  8&teachmeskills@gmail_.com8, ghk doc number JKI1423-1512-51LK phone number sdf e23 L+375(29)375-33-18L";
+        String REG = "\\+\\d{3}?.(\\d{2})?.\\d{7}|\\+\\d{3}?.(\\d{2})?.\\d{3}(-\\d{2}){2}|\\+\\d{3}?.(\\d{2})?.\\d{3}(-\\d{4})|[a-z0-9_-]+@[a-z0-9_\\.-]+\\.([a-z]{3})|(?:\\d{4}-){2}(\\d{2})";
 
         Pattern pattern = Pattern.compile(REG);
         Matcher matcher = pattern.matcher(text);
